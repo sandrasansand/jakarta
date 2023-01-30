cf272f30a5bfdd01c88888b9ffef447cd6e3c5d2
sandrasansand <sandrasaezcarbonell@gmail.com>
2023-01-30 20:23:07 +0100

    JakartaEE

---
+++ webapp-session/src/main/java/org/asaezc/apiservlet/webapp/session/services/LoginService.java
@@ -0,0 +1,9 @@
+package org.asaezc.apiservlet.webapp.session.services;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Optional;
+
+public interface LoginService {
+    Optional<String> getUsername(HttpServletRequest request);
+}
---
+++ webapp-session/src/main/java/org/asaezc/apiservlet/webapp/session/services/ProductoService.java
@@ -0,0 +1,9 @@
+package org.asaezc.apiservlet.webapp.session.services;
+
+import org.asaezc.apiservlet.webapp.session.models.Producto;
+
+import java.util.List;
+
+public interface ProductoService {
+    List<Producto> listar();
+}
