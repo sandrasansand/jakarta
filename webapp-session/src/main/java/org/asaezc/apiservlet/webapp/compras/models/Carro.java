69b88870864faa7d405a7adb81f78f0fb6e323e0
sandrasansand <sandrasaezcarbonell@gmail.com>
2023-01-31 00:20:20 +0100

    JakartaEE

---
+++ webapp-session/src/main/java/org/asaezc/apiservlet/webapp/compras/models/Carro.java
@@ -0,0 +1,64 @@
+package org.asaezc.apiservlet.webapp.compras.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Carro {
+    private List<ItemCarro> items;
+
+    //constructor vacio para inicializar la lista
+    public Carro() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addItemCarro(ItemCarro itemCarro) {
+        if (items.contains(itemCarro)) {
+            Optional<ItemCarro> optionalItemCarro = items.stream()
+                    .filter(i -> i.equals(itemCarro))
+                    .findAny();
+            if (optionalItemCarro.isPresent()) {
+                //validación ya sabemos que es un item == itemCarro .findAny() y queremos aumentar cantidad
+                ItemCarro i = optionalItemCarro.get();
+                //a la cantidad existente le sumamos 1
+                i.setCantidad(i.getCantidad()+1);
+            }
+        } else {
+            //es un item nuevo
+            this.items.add(itemCarro);
+        }
+    }
+    public List<ItemCarro> getItems() {
+        return items;
+    }
+
+    public int getTotal() {
+        return items.stream().mapToInt(ItemCarro::getImporte).sum();
+    }
+
+    //modificaciones para eliminar, actualizar
+
+    public void removeProductos(List<String> productoIds) {
+        if (productoIds != null) {
+            productoIds.forEach(this::removeProducto);
+            // que es lo mismo a:
+            // productoIds.forEach(productoId -> removeProducto(productoId));
+        }
+    }
+
+    public void removeProducto(String productoId) {
+        Optional<ItemCarro> producto = findProducto(productoId);
+        producto.ifPresent(itemCarro -> items.remove(itemCarro));
+    }
+
+    public void updateCantidad(String productoId, int cantidad) {
+        Optional<ItemCarro> producto = findProducto(productoId);
+        producto.ifPresent(itemCarro -> itemCarro.setCantidad(cantidad));
+    }
+
+    private Optional<ItemCarro> findProducto(String productoId) {
+        return  items.stream()
+                .filter(itemCarro -> productoId.equals(Long.toString(itemCarro.getProducto().getId())))
+                .findAny();
+    }
+}
