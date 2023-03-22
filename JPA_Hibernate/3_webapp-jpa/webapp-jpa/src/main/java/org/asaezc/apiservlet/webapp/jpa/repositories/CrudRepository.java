78aafad347b18503a7841ed7729f33c9a457823d
sandrasansand <sandrasaezcarbonell@gmail.com>
2023-03-22 16:52:11 +0100

    Update CrudRepository.java

--- JPA_Hibernate/3_webapp-jpa/webapp-jpa/src/main/java/org/asaezc/apiservlet/webapp/jpa/repositories/CrudRepository.java
+++ JPA_Hibernate/3_webapp-jpa/webapp-jpa/src/main/java/org/asaezc/apiservlet/webapp/jpa/repositories/CrudRepository.java
@@ -3,6 +3,7 @@ package org.asaezc.apiservlet.webapp.jpa.repositories;
 import java.util.List;
 
 public interface CrudRepository<T> {
+    //SQLexception -> Exception -> Jpa
     List<T> listar() throws Exception;
     T porId(Long id) throws Exception;
     void guardar(T t) throws Exception;
