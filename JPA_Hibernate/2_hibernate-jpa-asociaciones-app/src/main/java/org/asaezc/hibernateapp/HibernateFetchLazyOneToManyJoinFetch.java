06470aa7eda2371bc00f53b3291828096bb2ac4b
sandrasansand <sandrasaezcarbonell@gmail.com>
2023-03-22 02:52:30 +0100

    update

--- JPA_Hibernate/2_hibernate-jpa-asociaciones-app/src/main/java/org/asaezc/hibernateapp/HibernateFetchLazyOneToManyJoinFetch.java
+++ JPA_Hibernate/2_hibernate-jpa-asociaciones-app/src/main/java/org/asaezc/hibernateapp/HibernateFetchLazyOneToManyJoinFetch.java
@@ -7,7 +7,8 @@ import org.asaezc.hibernateapp.util.JpaUtil;
 public class HibernateFetchLazyOneToManyJoinFetch {
     public static void main(String[] args) {
         EntityManager em = JpaUtil.getEntityManager();
-
+		
+	//c.direcciones es el atributo, con left trae registros nulos y con fetch pobla el cliente
         Cliente cliente = em.createQuery("select c from Cliente c left outer join fetch c.direcciones left join fetch c.detalle where c.id=:id", Cliente.class)
                 .setParameter("id", 1L)
                 .getSingleResult();
--- JPA_Hibernate/2_hibernate-jpa-asociaciones-app/src/main/java/org/asaezc/hibernateapp/HibernateFetchManyToMany.java
+++ JPA_Hibernate/2_hibernate-jpa-asociaciones-app/src/main/java/org/asaezc/hibernateapp/HibernateFetchManyToMany.java
@@ -10,7 +10,7 @@ public class HibernateFetchManyToMany {
     public static void main(String[] args) {
 
         EntityManager em = JpaUtil.getEntityManager();
-
+	//a.cursos -> es el atributo de la clase Alumno
         List<Alumno> alumnos = em.createQuery("select distinct a from Alumno a left outer join fetch a.cursos", Alumno.class).getResultList();
         alumnos.forEach(a -> System.out.println(a.getNombre() + ", cursos: " + a.getCursos()));
         em.close();
