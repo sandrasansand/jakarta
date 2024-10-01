213a224999265867f88f1a9b1565dd74662ee175
Sandra Sáez Carbonell <90846642+sandrasansand@users.noreply.github.com>
2024-10-01 04:48:40 +0200

    Update CursoService.java

--- JAX-RS/webapp-jaxrs/src/main/services/CursoService.java
+++ JAX-RS/webapp-jaxrs/src/main/services/CursoService.java
@@ -1,1 +1,13 @@
-
+package org.asaez.webapp.jaxws.services;
+import jakarta.ejb.Local;
+import org.asaez.webapp.jaxws.models.Curso;
+import java.util.List;
+import java.util.Optional;
+//no necesario
+@Local
+public interface CursoService {
+    List<Curso> listar();
+    Curso guardar(Curso curso);
+    Optional<Curso> porId(Long id);
+    void eliminar(Long id);
+}
