48646a65be158ab41dca2129934e864029474c0d
Sandra Sáez Carbonell <90846642+sandrasansand@users.noreply.github.com>
2024-10-01 04:43:09 +0200

    Update CursoRepository.java

--- JAX-RS/webapp-jaxrs/src/main/repositories/CursoRepository.java
+++ JAX-RS/webapp-jaxrs/src/main/repositories/CursoRepository.java
@@ -1,1 +1,11 @@
+package org.asaez.webapp.jaxws.repositories;
+import org.asaez.webapp.jaxws.models.Curso;
+import java.util.List;
+
+public interface CursoRepository {
+    List<Curso> listar();
+    Curso guardar(Curso curso);
+    Curso porId(Long id);
+    void eliminar(Long id);
+}
 
