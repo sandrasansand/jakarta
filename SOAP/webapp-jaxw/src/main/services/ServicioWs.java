56af348c002c731fe58c743ac90388e0571b032b
Sandra Sáez Carbonell <90846642+sandrasansand@users.noreply.github.com>
2024-10-01 05:44:25 +0200

    Update ServicioWs.java

--- SOAP/webapp-jaxw/src/main/services/ServicioWs.java
+++ SOAP/webapp-jaxw/src/main/services/ServicioWs.java
@@ -1,1 +1,12 @@
+package org.asaezc.webapp.jaxws.services;
+import jakarta.jws.WebService;
+import org.asaezc.webapp.jaxws.models.Curso;
+import java.util.List;
+
+@WebService
+public interface ServicioWs {
+    String saludar(String persona);
+    List<Curso> listar();
+    Curso crear(Curso curso);
+}
 
